de7ff0b5995d844eeb29c9da183d029a91f89024
Suvigya kumawat <suvigyakum@gmail.com>
2024-09-11 22:54:16 +0530

    new change

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -14,8 +14,8 @@ export default function Signup(){
     const [lastName,setLastName]=useState("");
     const [userName,setUserName]=useState("");
     const [password,setPassword]=useState("");
-    return <div className="bg-slate-300 h-screen flex justify-center">
-            <div className="flex ">
+    return <div className="bg-slate-300 h-screen flex justify-center ">
+            <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading lable={"Signup"}/>
                     <SubHeading lable={"Enter your information to create an account"}/>
